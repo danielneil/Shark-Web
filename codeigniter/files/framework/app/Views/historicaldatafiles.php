9666eecb19a35e24cc13287e68143c3ccff48b50
Daniel Neil <danielneilhodder@gmail.com>
2022-02-10 00:27:38 +1100

    Update historicaldatafiles.php

--- codeigniter/files/framework/app/Views/historicaldatafiles.php
+++ codeigniter/files/framework/app/Views/historicaldatafiles.php
@@ -3,7 +3,7 @@
 	<div class="heroe">
 		<h1>Historical Data</h1>
 
-		<h2>Cached historical instrument data</h2>
+		<h2>Cached Historical Instrument Data</h2>
 	</div>
 </header>
 
@@ -14,6 +14,9 @@
 <h1>Index of historical data file cache</h1>
 	
 <pre><code>
+
+
+
 </code></pre>
 	
 </section>
