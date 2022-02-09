2cd78f733e25abbe82895d73e493f89b47bd69e2
Daniel Neil <danielneilhodder@gmail.com>
2022-02-09 16:22:32 +1100

    Update configlogfile.php

--- codeigniter/files/framework/app/Views/configlogfile.php
+++ codeigniter/files/framework/app/Views/configlogfile.php
@@ -243,25 +243,5 @@ echo nl2br(htmlentities(file_get_contents("/shark/log/config_gen.log"));
 
 </div>
 
-<!-- FOOTER: DEBUG INFO + COPYRIGHTS -->
-
-<footer>
-	<div class="environment">
-
-		<p>Page rendered in {elapsed_time} seconds</p>
-
-		<p>Environment: <?= ENVIRONMENT ?></p>
-
-	</div>
-
-	<div class="copyrights">
-
-		<p>&copy; <?= date('Y') ?> CodeIgniter Foundation. CodeIgniter is open source project released under the MIT
-			open source licence.</p>
-
-	</div>
-
-</footer>
-
 </body>
 </html>
