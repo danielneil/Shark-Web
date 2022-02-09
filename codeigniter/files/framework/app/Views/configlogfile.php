180407334d89e800e65e956bc480d7e348557b77
Daniel Neil <danielneilhodder@gmail.com>
2022-02-09 21:20:33 +1100

    Update configlogfile.php

--- codeigniter/files/framework/app/Views/configlogfile.php
+++ codeigniter/files/framework/app/Views/configlogfile.php
@@ -248,17 +248,3 @@ echo nl2br(htmlentities(file_get_contents("/shark/log/config_gen.log")));
 </section>
 
 </div>
-	
-<footer>
-
-
-	<div class="copyrights">
-
-		<p>&copy; <?= date('Y') ?> Shark - An Algorithmic Trading Platform. Shark is an open source project released under the GPL-3.0 License.</p>
-
-	</div>
-
-</footer>	
-	
-</body>
-</html>
