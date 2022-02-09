0c8f8a824e71ffcbe7508494350b86c8409c3892
Daniel Neil <danielneilhodder@gmail.com>
2022-02-09 17:40:51 +1100

    Update configlogfile.php

--- codeigniter/files/framework/app/Views/configlogfile.php
+++ codeigniter/files/framework/app/Views/configlogfile.php
@@ -216,10 +216,12 @@
 	<p>Shark's current configuration comprises of:</p>
 
 <pre><code>
-<h4>Instruments</h4>
+<h4>Instruments</h4>		
 Trading Instruments: 		20 
-Shark Plugins: 			3
-Trading Brokers: 		0
+Trading Groups:			<?php echo exec("grep -o -i 'instrument group' /shark/log/config_gen.log | wc -l") . PHP_EOL; ?> 
+Shark Plugins: 			<?php echo exec("grep -i 'Attached plugin' /shark/log/config_gen.log | awk '{ print $6 }' | sort | uniq | wc -l") . PHP_EOL; ?> 
+Shark Plugin Groups:		<?php echo exec("grep -o -i 'plugin group' /shark/log/config_gen.log | wc -l") . PHP_EOL; ?> 
+Trading Brokers: 		<?php echo exec("grep -o -i 'Created broker' /shark/log/config_gen.log | wc -l") . PHP_EOL; ?> 
 
 <h4>Git Configuration Repositories</h4>
 Shark Config Git REPO:		<?php echo exec("cd /shark/Shark-Config && git config --get remote.origin.url") . PHP_EOL; ?> 
