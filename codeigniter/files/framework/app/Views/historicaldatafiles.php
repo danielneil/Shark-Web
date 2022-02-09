3c9f37f7d2857bec171d3383af05a78e0eff7116
Daniel Neil <danielneilhodder@gmail.com>
2022-02-10 01:38:15 +1100

    Update historicaldatafiles.php

--- codeigniter/files/framework/app/Views/historicaldatafiles.php
+++ codeigniter/files/framework/app/Views/historicaldatafiles.php
@@ -25,7 +25,7 @@
 
         if ( count($files)-2) {
 
-		echo "Creation Date\tSize\tFile Name" . PHP_EOL ;
+		echo "Creation Date\t\t\tSize\tFile Name" . PHP_EOL ;
 		foreach (scandir($history_dir) as $f)
 		{
 			$fileName = $history_dir . '/'. $f;
