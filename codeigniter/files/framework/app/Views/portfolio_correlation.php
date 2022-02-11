561608d208bda517f62f3accaf1d3e2cb3852e24
Daniel Neil <danielneilhodder@gmail.com>
2022-02-11 13:57:39 +1100

    Update portfolio_correlation.php

--- codeigniter/files/framework/app/Views/portfolio_correlation.php
+++ codeigniter/files/framework/app/Views/portfolio_correlation.php
@@ -1,12 +1,20 @@
+<header>
+
+	<div class="heroe">
+		<h1>Portfolio Tools</h1>
+	</div>
+</header>
+
+<!-- CONTENT -->
+
 <section>
 
-	<h1>Correlation Matrix</h1>
-
-	<p>
-	A portfolio correlation matrix can help you see quickly what assets rise and fall together or which assets rise when others fall. The correlation matrix is derived from the underlying covariance matrix of asset returns, which is used to calculate portfolio risk or volatility.
-	</p>
-
-
-	<img src ="http://172.105.187.181/shark-web/portfolio/correlation_matrix.png" />
-	
+	<h4>Tool</h4>
+	<pre><code>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pulvinar nibh eu ultrices vulputate. Curabitur eu massa eu felis dapibus ornare. Phasellus finibus lacus vel purus vehicula facilisis. Nullam pellentesque ut lorem at molestie. Duis lobortis orci ac quam lacinia faucibus. Pellentesque eu tincidunt ipsum. Nunc ut nunc finibus, gravida dui eleifend, vehicula lectus. Nunc viverra tellus in diam dictum aliquam. Nunc lorem nulla, ullamcorper sed varius ut, tincidunt vel eros. Etiam sit amet accumsan massa. Donec pellentesque ultrices lorem, sed semper nisl aliquet eget. Sed sapien eros, bibendum eu ex in, interdum molestie erat. Cras mollis augue ut felis convallis hendrerit. Suspendisse porta justo vitae sem commodo tristique. Aenean eleifend libero vitae vulputate ultrices. Vestibulum malesuada consequat metus, ac convallis nibh interdum id.
+Nam ac ex semper, ullamcorper ex vel, commodo urna. Aliquam arcu mi, imperdiet ut tempus at, tempor ac quam. Sed eu felis tincidunt, imperdiet mauris eu, mattis lacus. Aenean nec auctor urna. Ut maximus leo nec ligula pharetra, non laoreet ipsum sodales. Nunc pellentesque diam velit, at pulvinar urna ultricies at. Maecenas vulputate congue urna, nec pretium magna porttitor ut. Praesent vitae dui neque.
+Mauris viverra dolor justo, eget vulputate mauris iaculis vel. Proin scelerisque pharetra pellentesque. Vivamus dui nisi, pretium sit amet tellus id, mollis elementum nisl. Ut sagittis arcu lacus, nec consectetur quam tempus malesuada. In sed nisi leo. Nullam fermentum quam quis augue mattis porttitor. Vestibulum venenatis vitae nunc non pellentesque. Suspendisse potenti. Nullam sit amet diam sed ante suscipit luctus sit amet a nisi. Vivamus in arcu sed nibh vehicula pulvinar. Sed quis hendrerit ex. Vivamus malesuada pellentesque accumsan. Vivamus faucibus leo quam. Morbi faucibus aliquam erat, eget lacinia risus mattis in. Aliquam enim ligula, posuere nec velit in, fringilla pulvinar augue.
+Praesent blandit cursus risus, ut ullamcorper neque. Aliquam a interdum tellus. Nam leo purus, maximus vitae molestie sed, mollis at nisl. Aenean sagittis, ligula rutrum tincidunt faucibus, velit velit porttitor est, tempor consectetur lacus leo in leo. Phasellus vel commodo enim. Maecenas ut blandit metus. Praesent pellentesque egestas luctus. Aliquam massa libero, suscipit nec urna ornare, feugiat sodales felis. Vestibulum consectetur lorem elit, elementum laoreet justo dictum non.
+Donec metus nisl, fermentum eget nibh quis, faucibus fermentum lorem. Curabitur fermentum molestie tellus eu eleifend. Quisque neque neque, commodo vitae ante ut, vulputate euismod metus. Quisque sit amet metus hendrerit libero tempor egestas ac dapibus risus. Etiam sed quam semper, luctus augue eu, tempor nunc. Curabitur convallis, est vel aliquet placerat, lectus enim hendrerit lectus, non aliquam elit augue in risus. Proin id enim mauris. Suspendisse eu neque in nunc euismod aliquet. Nam condimentum tempor tempor. Quisque condimentum, mauris cursus lobortis ornare, ipsum leo dapibus justo, a sollicitudin velit ante nec lacus. Aliquam augue turpis, suscipit at elit et, tempor rhoncus ipsum. Phasellus quis orci dignissim, porta orci id, sagittis neque. Suspendisse sit amet tincidunt purus, ut rhoncus mi. Aliquam sagittis purus non lobortis ultrices. Curabitur mi lorem, feugiat eget pulvinar et, vulputate ac eros.
+	</code></pre>
 </section>
